15c4e65add6ae96b91145d7d4bf4884f2ead88c9
shahinexy <shahinexy@gmail.com>
2024-11-24 23:18:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-4xl">After a long time start a new react project.</div>
+        
+    </>
+  )
+}
+
+export default App
