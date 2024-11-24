96455eddf076b4995709f43310b52cd30c0b14da
shahinexy <shahinexy@gmail.com>
2024-11-24 23:29:34 +0600

    react router implemented

---
+++ src/router/router.jsx
@@ -0,0 +1,14 @@
+
+
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div className="text-2xl text-green-300">React practices</div>,
+    },
+  ]);
+
+  export default router
